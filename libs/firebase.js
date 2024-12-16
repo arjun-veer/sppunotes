9bf4292bbc93b6ba530880d042d623821cfe300b
Arjun Veer <arjunveer7066@gmail.com>
2024-12-16 09:42:04 +0530

    Update firebase.js

--- libs/firebase.js
+++ libs/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth ,RecaptchaVerifier, signInWithPhoneNumber} from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDsgYyLsADi2H4cT7X2bJKppClHy-EV2lc",
-  authDomain: "sppunotesapp.firebaseapp.com",
-  projectId: "sppunotesapp",
-  storageBucket: "sppunotesapp.firebasestorage.app",
-  messagingSenderId: "735755459917",
-  appId: "1:735755459917:web:f416044acdc2cc56216b04",
-  measurementId: "G-BGNEYJTC2R"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
